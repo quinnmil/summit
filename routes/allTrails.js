5e43b9f2ff11dbcd7e59f87fe41b2bd58bd4971f
quinnmil <quinnmil@gmail.com>
2019-09-16 20:23:09 -0700

    return json

--- routes/allTrails.js
+++ routes/allTrails.js
@@ -5,8 +5,8 @@ const scraper = require('../controllers/allTrailsScraper.js');
 const ALLTRAILS_URL = 'https://www.alltrails.com/trail/us/oregon/south-sister-trail';
 
 
-router.get('/', () => {
-    scraper(ALLTRAILS_URL)
+router.get('/', (req, res) => {
+    res.json(scraper(ALLTRAILS_URL))
 })
 
 module.exports = router;
