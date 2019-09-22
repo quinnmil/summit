fea4c6d7291f92b1c2e7ac3b11327c82ba144e74
Quinn Milionis <35316732+quinnmil@users.noreply.github.com>
2019-09-21 21:06:07 -0700

    Update app.js
    Co-Authored-By: Miguel <39255541+margielamadman@users.noreply.github.com>

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/at', allTrailsRouter)
 app.use('/update', updateRouter);
 
 // catch 404 and forward to error handler
