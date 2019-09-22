8f834cfb478526aac6bf7cae3a784669cee63090
mn <39255541+margielamadman@users.noreply.github.com>
2019-09-22 00:57:41 -0700

    Add test route to plug into React

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Testing JSON */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({ test: `success` });
 });
 
 module.exports = router;
