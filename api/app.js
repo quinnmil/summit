00b61e7fa8081aa0a8676df33222f121f1191ec4
mn <39255541+margielamadman@users.noreply.github.com>
2019-09-21 18:28:21 -0700

    Remove templating stuff from express

--- api/app.js
+++ api/app.js
@@ -10,10 +10,6 @@ var allTrailsRouter = require('./routes/allTrails')
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -37,7 +33,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
