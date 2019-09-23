95bb5e870de25abc8c40763d839cac468ccfdff2
quinnmil <quinnmil@gmail.com>
2019-09-22 23:55:21 -0700

    reformat code with javascript standard

--- api/controllers/allTrailsScraper.js
+++ api/controllers/allTrailsScraper.js
@@ -1,114 +1,105 @@
-const request = require('request');
-const express = require('express');
-const cheerio = require('cheerio');
+const request = require('request')
+const express = require('express')
+const cheerio = require('cheerio')
 
 var config = require('../../config')
-const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(config.mongoURL, {useNewUrlParser: true});
+const MongoClient = require('mongodb').MongoClient
+const client = new MongoClient(config.mongoURL, { useNewUrlParser: true })
 
 const comments = function getComments (url, callback) {
-    getLatest(function (err, res) {
-        if (!err){
-            console.log("No error from get latest");
-            makeRequest(url, res, function (err, res) {
-                if (!err) {
-                    console.log("Good Response");
-                    // console.log("comments:" , comments)
-                    return (callback(false, res));
-                }
-                else {
-                    console.error("Bad response: ", err);
-                    return(callback(err, null));
-                }
-            })
-        }
-        else{
-            console.log("bad makeRequest")
-            return callback(err, null);
-        }
-    })
-}        
-
-const getLatest = function(callback) {
-    client.connect(function (err) {
+  getLatest(function (err, res) {
+    if (!err) {
+      console.log('No error from get latest')
+      makeRequest(url, res, function (err, res) {
         if (!err) {
-            console.log("Connected to Mongo Server");
-            const db = client.db("south_sister");
-            db.collection('Meta').findOne({}, {attribute: "latestComment"}, function (err, result) {
-            if (!err){
-                console.log("latest post:", result.timeStamp)
-                    var latest = result.timeStamp;
-                    client.close()
-                    return(callback(null, latest));
-            }
-            else {
-                console.log("meta lookup error", err);
-                return(callback(err, null));
-            }
-        })
+          console.log('Good Response')
+          // console.log("comments:" , comments)
+          return (callback(null, res))
+        } else {
+          console.error('Bad response: ', err)
+          return (callback(err, null))
+        }
+      })
+    } else {
+      console.log('bad makeRequest')
+      return callback(err, null)
     }
-})
+  })
 }
 
-
-const makeRequest = function(url, latestPost, callback){
-    request(url, (error, response, html) => {
-        if (!error && response.statusCode == 200) {
-            var comments = []
-            client.connect (function (err) {
-                if(!err){
-                    const $ = cheerio.load(html)
-                    $('#reviews div.feed-item').each((i, el) => {
-                        var author = $(el)
-                            .find('span.xlate-none').text() 
-                        var comment =$(el)
-                            .find('p.xlate-google').text().replace(/[\t\n\\]+/g,' ')
-                        // console.log("comment: " , comment);
-                        var datePublished = $(el)
-                            .find('span.subtext.pull-right').children('meta').attr('content')
-                        var timeList = $(el)
-                            .find('span.subtext.pull-right').text().split(' ')
-                        var timeSince;
-                        if (timeList[1] === "hours") {
-                            timeSince = timeList[0] * 60;
-                        }
-                        else if (timeList[1] === "days") {
-                            timeSince = timeList[0] * (24*60);
-                        }
-                        var d = new Date(Date.now() - (timeSince * 60000)) 
-                        // If the post is older than the latest in the db, stop here.
-
-                        // issue is here, can't break out of loop without returning false. 
-                        console.log("author: ",author," \t\t" ," d: ", d," \t", "latest: "," \t", latestPost);
-                        if (d > latestPost && comment !== ""){
-                            console.log("adding a comment from author: ",author, "from: ", d, "since the latest post is from: ", latestPost);
-                            console.log("text: ", comment)
-                            client.close();
-                            comments.push({
-                                timeStamp: d,
-                                source: "All Trails", 
-                                URL: url,
-                                text: comment,
-                                datePublished: datePublished,
-                                photo: null,
-                            });
-                            // callback(null, comments);
-                        }
-                    });
-                    client.close();
-                    return callback(null, comments);
-                }
-                else {
-                    console.log('client connection issue in scraper');
-                    return callback(err, null);
-                }
-            });
-            client.close()
+const getLatest = function (callback) {
+  client.connect(function (err) {
+    if (!err) {
+      console.log('Connected to Mongo Server')
+      const db = client.db('south_sister')
+      db.collection('Meta').findOne({}, { attribute: 'latestComment' }, function (err, result) {
+        if (!err) {
+          console.log('latest post:', result.timeStamp)
+          var latest = result.timeStamp
+          client.close()
+          return (callback(null, latest))
+        } else {
+          console.log('meta lookup error', err)
+          return (callback(err, null))
         }
-        else {
-            console.log("request error: ", error);
+      })
+    }
+  })
+}
+
+const makeRequest = function (url, latestPost, callback) {
+  request(url, (error, response, html) => {
+    if (!error && response.statusCode === 200) {
+      var comments = []
+      client.connect(function (err) {
+        if (!err) {
+          const $ = cheerio.load(html)
+          $('#reviews div.feed-item').each((i, el) => {
+            var author = $(el)
+              .find('span.xlate-none').text()
+            var comment = $(el)
+              .find('p.xlate-google').text().replace(/[\t\n\\]+/g, ' ')
+            // console.log("comment: " , comment);
+            var datePublished = $(el)
+              .find('span.subtext.pull-right').children('meta').attr('content')
+            var timeList = $(el)
+              .find('span.subtext.pull-right').text().split(' ')
+            var timeSince
+            if (timeList[1] === 'hours') {
+              timeSince = timeList[0] * 60
+            } else if (timeList[1] === 'days') {
+              timeSince = timeList[0] * (24 * 60)
+            }
+            var d = new Date(Date.now() - (timeSince * 60000))
+            // this will still process though every comment on page.
+            console.log('author: ', author, ' \t\t', ' d: ', d, ' \t', 'latest: ', ' \t', latestPost)
+            if (d > latestPost && comment !== '') {
+              console.log('adding a comment from author: ', author, 'from: ', d, 'since the latest post is from: ', latestPost)
+              console.log('text: ', comment)
+              client.close()
+              comments.push({
+                timeStamp: d,
+                source: 'All Trails',
+                URL: url,
+                text: comment,
+                datePublished: datePublished,
+                photo: null
+              })
+              // callback(null, comments);
+            }
+          })
+          client.close()
+          return callback(null, comments)
+        } else {
+          console.log('client connection issue in scraper')
+          return callback(err, null)
         }
-    })
+      })
+      client.close()
+    } else {
+      console.log('request error: ', error)
+    }
+  })
 }
 
 module.exports = comments
--- api/routes/update.js
+++ api/routes/update.js
@@ -1,107 +1,96 @@
 
-const express = require('express');
-const router = express.Router();
-const scraper = require('../controllers/allTrailsScraper.js');
+const express = require('express')
+const router = express.Router()
+const scraper = require('../controllers/allTrailsScraper.js')
 
 var config = require('../../config')
-const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(config.mongoURL, {useNewUrlParser: true, useUnifiedTopology: true });
+const MongoClient = require('mongodb').MongoClient
+const client = new MongoClient(config.mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const ALLTRAILS_URL = 'https://www.alltrails.com/trail/us/oregon/south-sister-trail';
+const ALLTRAILS_URL = 'https://www.alltrails.com/trail/us/oregon/south-sister-trail'
 
-
-router.get('/', function(req, res) {
-
-    scraper(ALLTRAILS_URL, function (err, data) {
+router.get('/', function (req, res) {
+  scraper(ALLTRAILS_URL, function (err, data) {
+    if (!err) {
+      ConnectMongo(function (err, db, collection) {
         if (!err) {
-            ConnectMongo(function(err, db, collection){
-                if (!err){
-                    insertComments(collection, data, function(err, result) {
-                        if (!err) {
-                            console.log("no error on insert ");
-                            
-                            updateLatest(db, result, function (err) {
-                                if (!err){
-                                    console.log("sucessfully updated latest timestamp");
-                                    client.close();
-                                    return res.send("success")
-                                }
-                                else {
-                                    console.log("there was a problem connecting: ", err);
-                                    client.close();
-                                    return res.send(err);
-                                }
-                            })
-                        }
-                        else {
-                            console.log("Error on insert: ", err);
-                            client.close();
-                            return res.send(err);
-                        }
-                    })
+          insertComments(collection, data, function (err, result) {
+            if (!err) {
+              console.log('no error on insert ')
+              updateLatest(db, result, function (err) {
+                if (!err) {
+                  console.log('sucessfully updated latest timestamp')
+                  client.close()
+                  return res.send('success')
+                } else {
+                  console.log('there was a problem connecting: ', err)
+                  client.close()
+                  return res.send(err)
                 }
-                else {
-                    console.log("Mongo connection error");
-                    return res.send(err)
-                }
-             
-            })
-        } else{
-            console.log("bad scrape, ouch");
-            return res.send(err);
+              })
+            } else {
+              console.log('Error on insert: ', err)
+              client.close()
+              return res.send(err)
+            }
+          })
+        } else {
+          console.log('Mongo connection error')
+          return res.send(err)
         }
-    })
+      })
+    } else {
+      console.log('bad scrape, ouch')
+      return res.send(err)
+    }
+  })
 })
 
-const ConnectMongo = function(callback){
-    client.connect(function (err) {
-        if (!err) {
-            console.log("Connected to Mongo Server");
-            const db = client.db("south_sister")
-            const collection = db.collection('Comments');
-            return callback(null, db, collection);
-        }
-        return callback(err, null, null);
-    })
+const ConnectMongo = function (callback) {
+  client.connect(function (err) {
+    if (!err) {
+      console.log('Connected to Mongo Server')
+      const db = client.db('south_sister')
+      const collection = db.collection('Comments')
+      return callback(null, db, collection)
+    }
+    return callback(err, null, null)
+  })
 }
 
-const insertComments = function(collection, comments, callback) {
-    if (comments.length !== 0){
-        collection.insertMany(comments, function (err, result){
-            if (!err) {
-                console.log("inserted", result.ops.length, "comments")
-                if (result){
-                    var latest = result.ops[0]['timeStamp'];
-                    return (callback(null, latest));
-                    // TODO return timestamp of newest comment
-                }
-            }
-            else {
-                return(callback(err, null));
-            }
-        })
-    }
-    else {
-        return(callback(null, null));   // this is okay?
+const insertComments = function (collection, comments, callback) {
+  if (comments.length !== 0) {
+    collection.insertMany(comments, function (err, result) {
+      if (!err) {
+        console.log('inserted', result.ops.length, 'comments')
+        if (result) {
+          var latest = result.ops[0].timeStamp
+          return (callback(null, latest))
+          // TODO return timestamp of newest comment
         }
+      } else {
+        return (callback(err, null))
+      }
+    })
+  } else {
+    return (callback(null, null)) // this is okay?
+  }
+}
+
+const updateLatest = function (db, latest, callback) {
+  console.log('calling updateLatest')
+  const meta = db.collection('Meta')
+  meta.updateOne({ attribute: 'latestComment' }, { $set: { timeStamp: latest } }, { upsert: true }, function (err, res) {
+    if (!err) {
+      console.log('Updated latest comment to: ', latest)
+      return (callback(null, res))
     }
+    console.log('error in updateLatest()', err)
+    return (callback(err))
+  })
+}
 
-const updateLatest = function(db, latest, callback) {
-    console.log('calling updateLatest')
-    // collection.find({}, {timeStamp: 1, _id: 0}).sort({timeStamp: -1}, function(err, cursor) {
-        // console.log("CURSOR: ",cursor);
-        // latest = cursor
-        // console.log('LATEST: ', latest.next());
-        const meta = db.collection('Meta');
-        meta.updateOne({attribute: "latestComment"}, { $set: {timeStamp: latest}}, {upsert: true}, function (err, res) {
-            if (!err) {
-                console.log("Updated latest comment to: ", latest);
-                return(callback(null, res));
-                }
-            console.log ('error in updateLatest()', err);
-            return(callback(err));
-            })
-        }
+module.exports = router
 
-
-module.exports = router;
+// // query to find the latest post
+// collection.find({}, {timeStamp: 1, _id: 0}).sort({timeStamp: -1}, function(err, cursor) {
