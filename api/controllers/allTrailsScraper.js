3ad14fec1839cb955fa8bde4c40a9cfd4427f749
quinnmil <quinnmil@gmail.com>
2019-09-23 23:42:20 -0700

    abstract mongo connections and validate code.

--- api/controllers/allTrailsScraper.js
+++ api/controllers/allTrailsScraper.js
@@ -2,15 +2,13 @@ const request = require('request')
 const express = require('express')
 const cheerio = require('cheerio')
 
-var config = require('../../config')
-const MongoClient = require('mongodb').MongoClient
-const client = new MongoClient(config.mongoURL, { useNewUrlParser: true })
+const mongo = require('../helpers/mongo')
 
 const comments = function getComments (url, callback) {
-  getLatest(function (err, res) {
+  getLatest(function (err, latest) {
     if (!err) {
       console.log('No error from get latest')
-      makeRequest(url, res, function (err, res) {
+      scrapeComments(url, latest, function (err, res) {
         if (!err) {
           console.log('Good Response')
           // console.log("comments:" , comments)
@@ -28,15 +26,19 @@ const comments = function getComments (url, callback) {
 }
 
 const getLatest = function (callback) {
-  client.connect(function (err) {
+  mongo.connect('south_sister', 'Meta', function (err, db, col) {
     if (!err) {
-      console.log('Connected to Mongo Server')
-      const db = client.db('south_sister')
-      db.collection('Meta').findOne({}, { attribute: 'latestComment' }, function (err, result) {
+      col.findOne({}, { attribute: 'latestComment' }, function (err, result) {
         if (!err) {
+          if (!result) {
+            console.log('No metadata, faking latest')
+            mongo.close()
+            var d = new Date(2015, 1, 1)
+            return (callback(null, d))
+          }
           console.log('latest post:', result.timeStamp)
           var latest = result.timeStamp
-          client.close()
+          mongo.close()
           return (callback(null, latest))
         } else {
           console.log('meta lookup error', err)
@@ -47,57 +49,52 @@ const getLatest = function (callback) {
   })
 }
 
-const makeRequest = function (url, latestPost, callback) {
+const scrapeComments = function (url, latestPost, callback) {
   request(url, (error, response, html) => {
     if (!error && response.statusCode === 200) {
       var comments = []
-      client.connect(function (err) {
-        if (!err) {
-          const $ = cheerio.load(html)
-          $('#reviews div.feed-item').each((i, el) => {
-            var author = $(el)
-              .find('span.xlate-none').text()
-            var comment = $(el)
-              .find('p.xlate-google').text().replace(/[\t\n\\]+/g, ' ')
-            // console.log("comment: " , comment);
-            var datePublished = $(el)
-              .find('span.subtext.pull-right').children('meta').attr('content')
-            var timeList = $(el)
-              .find('span.subtext.pull-right').text().split(' ')
-            var timeSince
-            if (timeList[1] === 'hours') {
-              timeSince = timeList[0] * 60
-            } else if (timeList[1] === 'days') {
-              timeSince = timeList[0] * (24 * 60)
-            }
-            var d = new Date(Date.now() - (timeSince * 60000))
-            // this will still process though every comment on page.
-            console.log('author: ', author, ' \t\t', ' d: ', d, ' \t', 'latest: ', ' \t', latestPost)
-            if (d > latestPost && comment !== '') {
-              console.log('adding a comment from author: ', author, 'from: ', d, 'since the latest post is from: ', latestPost)
-              console.log('text: ', comment)
-              client.close()
-              comments.push({
-                timeStamp: d,
-                source: 'All Trails',
-                URL: url,
-                text: comment,
-                datePublished: datePublished,
-                photo: null
-              })
-              // callback(null, comments);
-            }
+      const $ = cheerio.load(html)
+
+      $('#reviews div.feed-item').each((i, el) => {
+        var author = $(el)
+          .find('span.xlate-none').text()
+        var comment = $(el)
+          .find('p.xlate-google').text().replace(/[\t\n\\]+/g, ' ')
+          // console.log("comment: " , comment);
+        var datePublished = $(el)
+          .find('span.subtext.pull-right').children('meta').attr('content')
+        var timeList = $(el)
+          .find('span.subtext.pull-right').text().split(' ')
+
+        var timeSince = 0
+        if (timeList[1] === 'minutes') {
+          timeSince = timeList[0] * 60 // [minutes] * seconds
+        } else if (timeList[1] === 'hours') {
+          timeSince = timeList[0] * (60 * 60) // [hours] * minutes * seconds
+        } else if (timeList[1] === 'days') {
+          timeSince = timeList[0] * (24 * 60 * 60) // [days] * hours * minutes * seconds
+        }
+        var d = new Date(Date.now() - (timeSince * 1000)) // seconds * milliseconds
+        // this will still process though every comment on page.
+        console.log('author: ', author.padEnd(10), ' ', ' d: ', d, 'latest: ', latestPost)
+        if (d > latestPost && comment !== '') {
+          console.log(' ======= adding a comment from author: ', author.padEnd(10), 'from: ', d, 'since the latest post is from: ', latestPost)
+          console.log('text: ', comment)
+          comments.push({
+            timeStamp: d,
+            source: 'All Trails',
+            URL: url,
+            text: comment,
+            datePublished: datePublished,
+            photo: null
           })
-          client.close()
-          return callback(null, comments)
-        } else {
-          console.log('client connection issue in scraper')
-          return callback(err, null)
         }
       })
-      client.close()
+      // after looping through each comment
+      return callback(null, comments)
     } else {
       console.log('request error: ', error)
+      return callback(error, null)
     }
   })
 }
--- api/routes/update.js
+++ api/routes/update.js
@@ -2,35 +2,38 @@
 const express = require('express')
 const router = express.Router()
 const scraper = require('../controllers/allTrailsScraper.js')
-
-var config = require('../../config')
-const MongoClient = require('mongodb').MongoClient
-const client = new MongoClient(config.mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+const mongo = require('../helpers/mongo')
 
 const ALLTRAILS_URL = 'https://www.alltrails.com/trail/us/oregon/south-sister-trail'
 
 router.get('/', function (req, res) {
   scraper(ALLTRAILS_URL, function (err, data) {
     if (!err) {
-      ConnectMongo(function (err, db, collection) {
+      mongo.connect('south_sister', 'Comments', function (err, db, collection) {
         if (!err) {
           insertComments(collection, data, function (err, result) {
             if (!err) {
-              console.log('no error on insert ')
-              updateLatest(db, result, function (err) {
-                if (!err) {
-                  console.log('sucessfully updated latest timestamp')
-                  client.close()
-                  return res.send('success')
-                } else {
-                  console.log('there was a problem connecting: ', err)
-                  client.close()
-                  return res.send(err)
-                }
-              })
+              console.log('No error on insert')
+              if (result) {
+                updateLatest(db, result, function (err) {
+                  if (!err) {
+                    console.log('sucessfully updated latest timestamp')
+                    mongo.close()
+                    return res.send('success')
+                  } else {
+                    console.log('there was a problem connecting: ', err)
+                    mongo.close()
+                    return res.send(err)
+                  }
+                })
+              } else {
+                console.log('No new posts, not updating latest')
+                mongo.close()
+                return res.send('success')
+              }
             } else {
               console.log('Error on insert: ', err)
-              client.close()
+              mongo.close()
               return res.send(err)
             }
           })
@@ -46,34 +49,19 @@ router.get('/', function (req, res) {
   })
 })
 
-const ConnectMongo = function (callback) {
-  client.connect(function (err) {
-    if (!err) {
-      console.log('Connected to Mongo Server')
-      const db = client.db('south_sister')
-      const collection = db.collection('Comments')
-      return callback(null, db, collection)
-    }
-    return callback(err, null, null)
-  })
-}
-
 const insertComments = function (collection, comments, callback) {
   if (comments.length !== 0) {
     collection.insertMany(comments, function (err, result) {
       if (!err) {
         console.log('inserted', result.ops.length, 'comments')
-        if (result) {
-          var latest = result.ops[0].timeStamp
-          return (callback(null, latest))
-          // TODO return timestamp of newest comment
-        }
+        var latest = result.ops[0].timeStamp // this will be the most recent comment. might need to update later.
+        return (callback(null, latest))
       } else {
-        return (callback(err, null))
+        return callback(err, null)
       }
     })
   } else {
-    return (callback(null, null)) // this is okay?
+    return (callback(null, null))
   }
 }
 
