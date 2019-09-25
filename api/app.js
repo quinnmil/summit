8035028a1528e85f0210c196b5d0aabca5650cbf
quinnmil <quinnmil@gmail.com>
2019-09-25 00:24:30 -0700

    add south_sister api route

--- api/app.js
+++ api/app.js
@@ -1,43 +1,44 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+var cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var updateRouter = require('./routes/update');
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+var updateRouter = require('./routes/update')
+var southsisterRouter = require('./routes/south_sister')
 
-var app = express();
+var app = express()
 
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-// Routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/update', updateRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/update', updateRouter)
+app.use('/south_sister', southsisterRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  console.log(res.locals.message);
-  res.send(res.locals.message);
-});
+  res.status(err.status || 500)
+  console.log(res.locals.message)
+  res.send(res.locals.message)
+})
 
-module.exports = app;
+module.exports = app
---
+++ api/routes/south_sister.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const mongo = require('../helpers/mongo')
+
+router.get('/', function (req, res) {
+  mongo.connect('south_sister', 'Comments', function (err, db, collection) {
+    if (!err) {
+      var amount = typeof (req.query.amt) !== 'undefined' ? req.query.amt : 10
+      console.log('Getting ', amount, 'entries from database')
+      collection.find().sort({ timeStamp: -1 }).limit(amount).toArray(function (err, result) {
+        if (!err) {
+          console.log(result)
+          res.json(result)
+        } else {
+          console.log('error on find')
+          res.send(err)
+          mongo.close()
+        }
+      })
+    } else {
+      console.log('error no connect')
+      mongo.close()
+      res.send(err)
+    }
+  })
+})
+
+module.exports = router
