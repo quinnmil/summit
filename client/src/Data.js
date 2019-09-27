fbd969aea6105847ebd0519ad066876475f0d6e6
mn <39255541+margielamadman@users.noreply.github.com>
2019-09-27 03:13:09 -0700

    Update API call, use axios

--- client/src/Data.js
+++ client/src/Data.js
@@ -1,29 +1,55 @@
 import React from 'react'
+import axios from 'axios' // Used for making AJAX requests
 
-const API = 'https://hn.algolia.com/api/v1/search?query=';
-const DEFAULT_QUERY = 'google';
+const API = 'https://jsonplaceholder.typicode.com/users'
 
 class Data extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { apiResponse: '' }
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    }
   }
 
   callAPI () {
-    /* global fetch */
-    fetch('http://localhost:9000/users')
-      .then(res => res.json()) /* get the JSON response */
-      .then(data => this.setState({ apiResponse: data }))
+    // Make a get request
+    axios.get(API)
+      .then(response => {
+        // Handle success
+        this.setState({
+          isLoaded: true,
+          items: response.data
+        })
+      }
+      )
+      .catch(function (error) {
+        // Handle error
+        console.log(error)
+      })
+      .finally(function () {
+        // Always executed
+      })
   }
 
   componentDidMount () {
+    // Load data into component here
+    // this allows you load it into
+    // the component's state
     this.callAPI()
   }
 
   render () {
-    const { apiResponse } = this.state
+    const { items } = this.state
     return (
-      <p>{apiResponse.test}</p>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.name}: {item.email}
+          </li>
+        ))}
+      </ul>
     )
   }
 }
