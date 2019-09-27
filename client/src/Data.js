013249de2f338bb869bddcc7d92af5ae66d3c30a
mn <39255541+margielamadman@users.noreply.github.com>
2019-09-26 23:42:21 -0700

    Lint client/

--- client/src/Data.js
+++ client/src/Data.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React from 'react'
 
 class Data extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {apiResponse: ""};
-    }
+  constructor (props) {
+    super(props)
+    this.state = { apiResponse: '' }
+  }
 
-    callAPI() {
-        fetch("http://localhost:9000/users")
-            .then(res => res.json()) /* get the JSON response */
-            .then(data => this.setState({ apiResponse: data }));
-    }
+  callAPI () {
+    /* global fetch */
+    fetch('http://localhost:9000/users')
+      .then(res => res.json()) /* get the JSON response */
+      .then(data => this.setState({ apiResponse: data }))
+  }
 
-    componentDidMount() {
-        this.callAPI();
-    }
+  componentDidMount () {
+    this.callAPI()
+  }
 
-    render() {
-        const { apiResponse } = this.state;
-        return(
-            <p>{apiResponse.test}</p>
-        );
-    }
+  render () {
+    const { apiResponse } = this.state
+    return (
+      <p>{apiResponse.test}</p>
+    )
+  }
 }
 
-export default Data;
+export default Data
