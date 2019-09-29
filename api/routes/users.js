9372d7ef1c6430510320d5dbdc023bc7d755937a
mn <39255541+margielamadman@users.noreply.github.com>
2019-09-29 14:24:29 -0700

    Remove users route, no longer needed

--- api/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-/* Testing JSON */
-router.get('/', function (req, res, next) {
-  res.json({ test: 'success' })
-})
-
-module.exports = router
