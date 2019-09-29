a955959a7c40a6f9cbcbbff907faf259a02cc059
mn <39255541+margielamadman@users.noreply.github.com>
2019-09-29 15:53:24 -0700

    Add call to weather api endpoint

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import Data from './Data'
+import Weather from './Weather'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Weather />, document.getElementById('weather'))
 ReactDOM.render(<Data />, document.getElementById('data'))
