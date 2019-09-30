b8de2130e9d552687b67bbc158fad4af99ddfc9a
mn <39255541+margielamadman@users.noreply.github.com>
2019-09-29 19:15:18 -0700

    Redesign

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
 import Data from './components/Data'
 import Weather from './components/Weather'
 
-ReactDOM.render(<App />, document.getElementById('root'))
 ReactDOM.render(<Weather />, document.getElementById('weather'))
 ReactDOM.render(<Data />, document.getElementById('data'))
