dea3bd62d8a5e6bd03d897670a19110c82792070
mn <39255541+margielamadman@users.noreply.github.com>
2019-09-29 20:33:02 -0700

    Update comment cards

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -4,15 +4,11 @@ const Comments = ({ comments }) => {
   return (
     <div>
       {comments.map((comment) => (
-        <li key={comment._id}>
-          <div className='card'>
-            <div className='card-body'>
-              <h5 className='card-title'>{comment.datePublished}</h5>
-              <h6 className='card-subtitle mb-2 text-muted'>{comment.source}</h6>
-              <p className='card-text'>{comment.text}</p>
-            </div>
-          </div>
-        </li>
+        <div key={comment._id}>
+          <p id='comment'>{comment.text}</p>
+          <small className='text-muted'>Posted on {comment.datePublished} Source: {comment.source}</small>
+          <hr />
+        </div>
       ))}
     </div>
   )
