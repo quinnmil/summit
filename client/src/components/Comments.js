d83d2057eab22bbe672eb0d1be0d03feefeaa930
mn <39255541+margielamadman@users.noreply.github.com>
2019-09-29 19:15:18 -0700

    Redesign

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -3,14 +3,13 @@ import React from 'react'
 const Comments = ({ comments }) => {
   return (
     <div>
-      <center><h1>Comments</h1></center>
       {comments.map((comment) => (
         <li key={comment._id}>
-          <div class='card'>
-            <div class='card-body'>
-              <h5 class='card-title'>{comment.datePublished}</h5>
-              <h6 class='card-subtitle mb-2 text-muted'>{comment.source}</h6>
-              <p class='card-text'>{comment.text}</p>
+          <div className='card'>
+            <div className='card-body'>
+              <h5 className='card-title'>{comment.datePublished}</h5>
+              <h6 className='card-subtitle mb-2 text-muted'>{comment.source}</h6>
+              <p className='card-text'>{comment.text}</p>
             </div>
           </div>
         </li>
