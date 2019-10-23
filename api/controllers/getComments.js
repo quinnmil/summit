cc47bfb7734af2a8dc39e8adcdb8eba4a168bd47
quinnmil <quinnmil@gmail.com>
2019-10-22 22:44:00 -0700

    small changes to getComments. prep for merge.

--- api/controllers/getComments.js
+++ api/controllers/getComments.js
@@ -1,20 +1,29 @@
 const ATScraper = require('../controllers/allTrailsScraper.js')
-const RedditScraper = requrie('../controllers/redditScraper.js')
+const RedditScraper = require('../controllers/redditScraper.js')
 
 const ALLTRAILS_URL = 'https://www.alltrails.com/trail/us/oregon/south-sister-trail'
 const REDDIT_URL = 'https://www.reddit.com/r/Mountaineering/search.json?q=south sister&limit=10&restrict_sr=true'
 
 function scrapeAllSources (mountain, trail) {
-  var allComments = [] // all comment objects from all sources
-  // TODO need to move the URLs into a 'sources' file.
-  ATScraper(ALLTRAILS_URL, function (err, data) {
-    if (!err) {
-      allComments.push(data)
-    } else { console.log('All Trails scraped to get data for ', mountain, trail) }
+  return new Promise((resolve, reject) => {
+    var allComments = [] // all comment objects from all sources
+    // TODO need to move the URLs into a 'sources' file.
+    ATScraper(ALLTRAILS_URL, function (err, data) {
+      if (!err) {
+        allComments.push(data)
+      } else {
+        console.log('All Trails failed to get data for ', mountain, trail)
+        reject(err)
+      }
+    })
+    RedditScraper(mountain, 20).then(result => {
+      console.log('received comments from reddit scraper')
+      allComments.push(result)
+      resolve(allComments)
+    })
+      .catch(error =>
+        console.log(error))
   })
-  RedditScraper(mountain, 20).then(result => {
-    allComments.push(result)
-  })
-    .catch(error =>
-      console.log(error))
 }
+
+module.exports = scrapeAllSources
